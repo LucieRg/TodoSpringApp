807060a7c0975d18eb413a2268174f9d667f3387
LucieRg <129874296+LucieRg@users.noreply.github.com>
2024-07-29 13:08:55 +0200

    Update Exercice6SpringApplication.java

--- src/main/java/org/example/exercice6spring/Exercice6SpringApplication.java
+++ src/main/java/org/example/exercice6spring/Exercice6SpringApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Exercice6SpringApplication {
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Exercice6SpringApplication.class, args);
